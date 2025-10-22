c4371f451a9265ac26fe80265234dc0f02d13689
Manuel <mvargasbejar@gmail.com>
2025-10-22 20:29:41 +0200

    Subimos archivos gitignore y pendiente

--- hola_git.java
+++ hola_git.java
@@ -1,4 +1,4 @@
-public class Hola_git {
+public class hola_git {
     public static void main(String[]args){
         System.out.println("Hola git");
     }
--- hola_git2.java
+++ hola_git2.java
@@ -1,4 +1,4 @@
-public class Hola_git {
+public class hola_git2 {
     public static void main(String[]args){
         System.out.println("Hola git2");
     }
